3dbf8c170f10d3dcbd77247172a34f48df6508fb
kiranbandi <bvenkatkiran@gmail.com>
2018-05-17 14:59:42 -0600

    chromosome tracks basic structure complete

---
+++ src/infoVisualization.js
@@ -0,0 +1,68 @@
+import * as d3 from 'd3';
+
+export default function(container, information) {
+
+    let infoContainer = container.append('div')
+        .attr('class', 'subContainer infoContainer col s12 m6 center-align'),
+
+        graphicContainer = container.append('div')
+        .attr('class', 'subContainer graphicContainer col s12 m6 center-align');
+
+    infoContainer.append('h4')
+        .text('Information')
+        .attr('class', 'red-text text-lighten-2');
+
+    graphicContainer.append('h4')
+        .text('Share of Collinear Genes')
+        .attr('class', 'red-text text-lighten-2');
+
+    infoContainer
+        .selectAll('.subInfoTitle')
+        .data(information.parameters)
+        .enter()
+        .append('h6')
+        .attr('class', 'subInfoTitle')
+        .text((d) => d[0] + " : " + d[1]);
+
+
+    let width = (graphicContainer.node().clientWidth) / 2,
+        radius = width / 2;
+
+    let color = d3.scaleOrdinal()
+        .range(['#be1522', '#1d1d1b']);
+
+    let vis = graphicContainer
+        .append("svg")
+        .data([
+            [information.stats.percentage, 100 - information.stats.percentage]
+        ])
+        .attr("width", width)
+        .attr("height", width / 2)
+        .append("svg:g")
+        .attr('transform', 'translate(' + radius + ',' + radius + ')'),
+
+        arc = d3.arc()
+        .innerRadius(radius / 1.25)
+        .outerRadius(radius),
+
+        pie = d3.pie()
+        .startAngle(-90 * (Math.PI / 180))
+        .endAngle(90 * (Math.PI / 180))
+        .padAngle(.02)
+        .sort(null)
+        .value((d) => d),
+
+        arcs = vis.selectAll("g.slice")
+        .data(pie)
+        .enter()
+        .append("svg:g")
+        .attr("class", "slice")
+        .append("svg:path")
+        .attr("fill", function(d, i) { return color(i); })
+        .attr("d", arc),
+
+        textFiller = vis.append('svg:text')
+        .attr('class', 'pieText')
+        .attr('transform', 'translate(-' + 0.2 * radius + ',-' + 0.2 * radius + ')')
+        .text(information.stats.percentage + " %");
+}
--- src/linearView.js
+++ src/linearView.js
@@ -1,10 +1,8 @@
 import * as d3 from 'd3';
 import _ from 'lodash';
+import infoVisualization from './infoVisualization'
 
 export default function(information, alignmentList, genomeLibrary, chromosomeMap) {
-
-    let { clientWidth } = document.body,
-        width = clientWidth < window.innerHeight ? clientWidth : window.innerHeight;
 
     let linearViewMainContainer = d3.select("#root")
         .append('div')
@@ -14,9 +12,83 @@ export default function(information, alignmentList, genomeLibrary, chromosomeMap
         .attr('class', 'headContainer row'),
 
         filterConainter = headContainer.append('div')
-        .attr('class', 'subContainer filterConainter col s12');
+        .attr('class', 'subContainer filterConainter col s12'),
 
-    makeInfoVisualization(headContainer, information);
+        width = linearViewMainContainer.node().clientWidth,
+
+        linearViewVis = linearViewMainContainer.append('svg')
+        .attr('class', 'linearViewVis')
+        .attr('height', width)
+        .attr('width', width);
+
+    infoVisualization(headContainer, information);
+
+    let sourceKeys = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+        targetKeys = [11, 12, 13, 14, 15, 16, 17, 18, 19],
+        sourceWidth = [],
+        sumStore = 0,
+        targetStore = 0,
+        targetWidth = [];
+    //  reduce throwing an error in prod build so using each instead
+    _.each(sourceKeys, (chromosomeKey) => {
+        let chromosomeWidth = chromosomeMap.get(chromosomeKey).end - chromosomeMap.get(chromosomeKey).start
+        sourceWidth.push([sumStore, chromosomeWidth]);
+        sumStore += chromosomeWidth;
+    })
+    _.each(targetKeys, (chromosomeKey) => {
+        let chromosomeWidth = chromosomeMap.get(chromosomeKey).end - chromosomeMap.get(chromosomeKey).start
+        targetWidth.push([targetStore, chromosomeWidth]);
+        targetStore += chromosomeWidth;
+    })
+
+    let sourceScaleFactor = (width * 0.90) / _.sumBy(sourceWidth, (o) => o[1]),
+        targetScaleFactor = (width * 0.90) / _.sumBy(targetWidth, (o) => o[1]),
+        sourcePaddingTop = width * 0.1;
+
+    let linepadding = 40;
+
+    let distanceBetweenTracks = 0.1 * width;
+
+    let sourceChromosomeMarkers = linearViewVis
+        .append('g')
+        .attr('class', 'sourceMarkerContainer')
+        .selectAll('.sourceMarker')
+        .data(sourceWidth)
+        .enter()
+        .append('line')
+        .attr('class', 'sourceMaker')
+        .style('stroke', 'green')
+        .style('stroke-width', '1em')
+        .style('stroke-linecap', 'round')
+        .attr('x1', (d, i) => {
+            return (d[0] * sourceScaleFactor) + linepadding;
+        })
+        .attr('y1', sourcePaddingTop)
+        .attr('x2', (d, i) => {
+            return (d[0] + d[1]) * sourceScaleFactor;
+        })
+        .attr('y2', sourcePaddingTop)
+
+
+    let targetChromosomeMarkers = linearViewVis
+        .append('g')
+        .attr('class', 'targetMarkerContainer')
+        .selectAll('.targetMarker')
+        .data(targetWidth)
+        .enter()
+        .append('line')
+        .style('stroke', 'black')
+        .style('stroke-width', '1em')
+        .style('stroke-linecap', 'round')
+        .attr('x1', (d, i) => {
+            return (d[0] * targetScaleFactor) + linepadding;
+        })
+        .attr('y1', sourcePaddingTop + distanceBetweenTracks)
+        .attr('x2', (d, i) => {
+            return (d[0] + d[1]) * targetScaleFactor;
+        })
+        .attr('y2', sourcePaddingTop + distanceBetweenTracks)
+
 }
 
 function parseAlignmentDetails(alignmentDetails) {
@@ -29,71 +101,4 @@ function parseAlignmentDetails(alignmentDetails) {
         'source': alignmentDetailsList[6].split('&')[0].trim(),
         'target': alignmentDetailsList[6].split('&')[1].trim()
     };
-}
-
-function makeInfoVisualization(headContainer, information) {
-
-    let infoContainer = headContainer.append('div')
-        .attr('class', 'subContainer infoContainer col s12 m6 center-align'),
-
-        graphicContainer = headContainer.append('div')
-        .attr('class', 'subContainer graphicContainer col s12 m6 center-align');
-
-    infoContainer.append('h4')
-        .text('Information')
-        .attr('class', 'red-text text-lighten-2');
-
-    graphicContainer.append('h4')
-        .text('Share of Collinear Genes')
-        .attr('class', 'red-text text-lighten-2');
-
-    infoContainer
-        .selectAll('.subInfoTitle')
-        .data(information.parameters)
-        .enter()
-        .append('h6')
-        .attr('class', 'subInfoTitle')
-        .text((d) => d[0] + " : " + d[1]);
-
-
-    let width = (graphicContainer.node().clientWidth) / 2,
-        radius = width / 2;
-
-    let color = d3.scaleOrdinal()
-        .range(['#be1522', '#1d1d1b']);
-
-    let vis = graphicContainer
-        .append("svg")
-        .data([
-            [information.stats.percentage, 100 - information.stats.percentage]
-        ])
-        .attr("width", width)
-        .attr("height", width)
-        .append("svg:g")
-        .attr('transform', 'translate(' + radius + ',' + radius + ')'),
-
-        arc = d3.arc()
-        .innerRadius(radius / 1.25)
-        .outerRadius(radius),
-
-        pie = d3.pie()
-        .startAngle(-90 * (Math.PI / 180))
-        .endAngle(90 * (Math.PI / 180))
-        .padAngle(.02)
-        .sort(null)
-        .value((d) => d),
-
-        arcs = vis.selectAll("g.slice")
-        .data(pie)
-        .enter()
-        .append("svg:g")
-        .attr("class", "slice")
-        .append("svg:path")
-        .attr("fill", function(d, i) { return color(i); })
-        .attr("d", arc),
-
-        textFiller = vis.append('svg:text')
-        .attr('class', 'pieText')
-        .attr('transform', 'translate(-' + 0.2 * radius + ',-' + 0.2 * radius + ')')
-        .text(information.stats.percentage + " %");
 }
