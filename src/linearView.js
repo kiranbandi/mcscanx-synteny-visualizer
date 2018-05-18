9c0c2adbd4871640336accdf5b0cc9b077fa942a
kiranbandi <bvenkatkiran@gmail.com>
2018-05-18 12:55:59 -0600

    basic linking complete , modularise pending

--- src/linearView.js
+++ src/linearView.js
@@ -1,8 +1,10 @@
 import * as d3 from 'd3';
 import _ from 'lodash';
-import infoVisualization from './infoVisualization'
+import infoVisualization from './infoVisualization';
 
 export default function(information, alignmentList, genomeLibrary, chromosomeMap) {
+
+    let colorScale = d3.schemeCategory10;
 
     let linearViewMainContainer = d3.select("#root")
         .append('div')
@@ -27,43 +29,34 @@ export default function(information, alignmentList, genomeLibrary, chromosomeMap
         targetKeys = [11, 12, 13, 14, 15, 16, 17, 18, 19];
 
 
-    let sourceMarkerInfo = drawmarkers(linearViewVis, sourceKeys, chromosomeMap, 100, 'source', 'red'),
-        targetMarkerInfo = drawmarkers(linearViewVis, targetKeys, chromosomeMap, 350, 'target', 'black');
+    let sourceMarkerInfo = drawmarkers(linearViewVis, sourceKeys, chromosomeMap, 100, 'source', true, colorScale),
+        targetMarkerInfo = drawmarkers(linearViewVis, targetKeys, chromosomeMap, 350, 'target', false, colorScale);
 
     let filteredAlignmentList = filterAlignmentList(sourceKeys, targetKeys, alignmentList);
 
+    let linksVis = _.map(filteredAlignmentList, (alignment) => {
 
-    //     _.each(_.sortBy(filteredAlignmentList, (o) => o.count), (alignment) => {
+        let firstLink = alignment.links[0];
 
-    //         let firstLink = alignment.links.slice(0, 1)[0],
-    //             lastLink = alignment.links.slice(-1)[0];
+        if (alignment.isFlipped) {
 
-    //         let sourceMarkerStartForChromosome = sourceMarkerInfo.markerCoordinatesMap[alignment.sourceKey];
-    //         let targetMarkerStartForChromosome = targetMarkerInfo.markerCoordinatesMap[alignment.targetKey];
+            let sourceMarkerStartForChromosome = sourceMarkerInfo.markerCoordinatesMap[alignment.targetKey];
+            let targetMarkerStartForChromosome = targetMarkerInfo.markerCoordinatesMap[alignment.sourceKey];
 
-    //         let sourceChromosome = chromosomeMap.get(alignment.sourceKey);
-    //         let targetChromosome = chromosomeMap.get(alignment.targetKey);
+            let sourceChromosome = chromosomeMap.get(alignment.targetKey);
+            let targetChromosome = chromosomeMap.get(alignment.sourceKey);
 
-    //         // polygon coordinates numbering start in a circular fashion from top left to bottom left
-    //         let polygonCoordinates_1 = genomeLibrary.get(firstLink.source).start;
-    //         let polygonCoordinates_2 = genomeLibrary.get(lastLink.source).start;
-    //         let polygonCoordinates_3 = genomeLibrary.get(firstLink.target).start;
-    //         let polygonCoordinates_4 = genomeLibrary.get(lastLink.target).start;
+            // polygon coordinates numbering start in a circular fashion from top left to bottom left
+            let polygonCoordinates_1 = genomeLibrary.get(firstLink.target).start;
+            let polygonCoordinates_4 = genomeLibrary.get(firstLink.source).start;
 
-    //         polygonCoordinates_1 = ((polygonCoordinates_1 - sourceChromosome.start) / (sourceChromosome.end - sourceChromosome.start))
-    //         polygonCoordinates_2 = ((polygonCoordinates_2 - sourceChromosome.start) / (sourceChromosome.end - sourceChromosome.start))
+            polygonCoordinates_1 = sourceMarkerStartForChromosome[0] + ((polygonCoordinates_1 - sourceChromosome.start) * sourceMarkerInfo.scaleFactor)
+            polygonCoordinates_4 = targetMarkerStartForChromosome[0] + ((polygonCoordinates_4 - targetChromosome.start) * targetMarkerInfo.scaleFactor)
 
-    //         polygonCoordinates_3 = ((polygonCoordinates_3 - targetChromosome.start) / (targetChromosome.end - targetChromosome.start))
-    //         polygonCoordinates_4 = ((polygonCoordinates_4 - targetChromosome.start) / (targetChromosome.end - targetChromosome.start))
+            return [alignment.targetKey, polygonCoordinates_1, polygonCoordinates_4];
 
-    //         console.log(alignment.type, polygonCoordinates_2 - polygonCoordinates_1, polygonCoordinates_4 - polygonCoordinates_3);
-    //     })
-    // }
+        }
 
-
-    _.each(filteredAlignmentList, (alignment) => {
-
-        let firstLink = alignment.links.slice(0, 1)[0];
 
         let sourceMarkerStartForChromosome = sourceMarkerInfo.markerCoordinatesMap[alignment.sourceKey];
         let targetMarkerStartForChromosome = targetMarkerInfo.markerCoordinatesMap[alignment.targetKey];
@@ -73,30 +66,45 @@ export default function(information, alignmentList, genomeLibrary, chromosomeMap
 
         // polygon coordinates numbering start in a circular fashion from top left to bottom left
         let polygonCoordinates_1 = genomeLibrary.get(firstLink.source).start;
-        let polygonCoordinates_2 = genomeLibrary.get(lastLink.source).start;
-        let polygonCoordinates_3 = genomeLibrary.get(firstLink.target).start;
-        let polygonCoordinates_4 = genomeLibrary.get(lastLink.target).start;
+        let polygonCoordinates_4 = genomeLibrary.get(firstLink.target).start;
 
-        polygonCoordinates_1 = ((polygonCoordinates_1 - sourceChromosome.start) / (sourceChromosome.end - sourceChromosome.start))
-        polygonCoordinates_2 = ((polygonCoordinates_2 - sourceChromosome.start) / (sourceChromosome.end - sourceChromosome.start))
+        polygonCoordinates_1 = sourceMarkerStartForChromosome[0] + ((polygonCoordinates_1 - sourceChromosome.start) * sourceMarkerInfo.scaleFactor)
+        polygonCoordinates_4 = targetMarkerStartForChromosome[0] + ((polygonCoordinates_4 - targetChromosome.start) * targetMarkerInfo.scaleFactor)
 
-        polygonCoordinates_3 = ((polygonCoordinates_3 - targetChromosome.start) / (targetChromosome.end - targetChromosome.start))
-        polygonCoordinates_4 = ((polygonCoordinates_4 - targetChromosome.start) / (targetChromosome.end - targetChromosome.start))
-
-        console.log(alignment.type, polygonCoordinates_2 - polygonCoordinates_1, polygonCoordinates_4 - polygonCoordinates_3);
+        return [alignment.sourceKey, polygonCoordinates_1, polygonCoordinates_4];
     })
+
+    linearViewVis.selectAll('alignmentLink')
+        .data(linksVis)
+        .enter()
+        .append('line')
+        .style('stroke', (d) => {
+            return colorScale[d[0] - 1];
+        })
+        .style('opacity', 0.50)
+        .attr('class', 'alignmentLink')
+        .attr('x1', (d) => d[1])
+        .attr('y1', 100)
+        .attr('x2', (d) => d[2])
+        .attr('y2', 350)
 }
 
 
 function filterAlignmentList(sourceKeys, targetKeys, alignmentList) {
+    let sourceKeysCopy = [1, 3, 9];
     return _.filter(alignmentList, (alignment) => {
-        let sourceAndTarget = alignment.sourceKey && alignment.targetKey && (sourceKeys.indexOf(alignment.sourceKey) > -1) && (targetKeys.indexOf(alignment.targetKey) > -1),
-            targetAndSrouce = alignment.sourceKey && alignment.targetKey && (sourceKeys.indexOf(alignment.targetKey) > -1) && (targetKeys.indexOf(alignment.sourceKey) > -1);
-        return sourceAndTarget;
+        let sourceAndTarget = alignment.sourceKey && alignment.targetKey && (sourceKeysCopy.indexOf(alignment.sourceKey) > -1) && (targetKeys.indexOf(alignment.targetKey) > -1),
+            targetAndSrouce = alignment.sourceKey && alignment.targetKey && (sourceKeysCopy.indexOf(alignment.targetKey) > -1) && (targetKeys.indexOf(alignment.sourceKey) > -1);
+
+        if (targetAndSrouce) {
+            alignment.isFlipped = true;
+        }
+
+        return (sourceAndTarget || targetAndSrouce);
     });
 }
 
-function drawmarkers(svgContainer, keys, chromosomeCollection, positionFromTop, markerIndex, markerColor = 'green') {
+function drawmarkers(svgContainer, keys, chromosomeCollection, positionFromTop, markerIndex, isSource = false, colorScale) {
 
     let chromosomeWidthList = [],
         sumStore = 0,
@@ -118,21 +126,51 @@ function drawmarkers(svgContainer, keys, chromosomeCollection, positionFromTop, 
         return [(chromosomeWidth[0] * scaleFactor) + ((index + 1) * markerPadding), ((chromosomeWidth[0] + chromosomeWidth[1]) * scaleFactor) + ((index + 1) * markerPadding)];
     })
 
-    let chromosomeMarkers = svgContainer
+    let markerContainer = svgContainer
         .append('g')
-        .attr('class', 'markerContainer' + '-' + markerIndex)
+        .attr('class', 'markerContainer' + '-' + markerIndex);
+
+    let chromosomeMarkers = markerContainer
         .selectAll('.marker')
         .data(markerCoordinates)
         .enter()
         .append('line')
         .attr('class', 'marker')
-        .style('stroke', markerColor)
+        .style('stroke', (d, i) => {
+            if (isSource) {
+                return colorScale[i];
+            } else {
+                return (i % 2) == 0 ? 'black' : 'grey';
+            }
+        })
         .style('stroke-width', '0.75em')
         .style('stroke-linecap', 'round')
         .attr('x1', (d) => d[0])
         .attr('y1', positionFromTop)
         .attr('x2', (d) => d[1])
         .attr('y2', positionFromTop);
+
+
+    let chromosomeMarkerTexts = markerContainer
+        .selectAll('.markerText')
+        .data(keys)
+        .enter()
+        .append('text')
+        .attr('class', 'markerText')
+        .style('color', (d, i) => {
+            return 'black';
+        })
+        .attr('y', (d) => {
+            return (positionFromTop + ((isSource ? -1 : 1) * 25))
+        })
+        .text((d) => {
+            return "Chr " + d;
+        })
+        .attr('x', function(d, i) {
+            let position = ((markerCoordinates[i][0] + markerCoordinates[i][1]) / 2);
+            // subtracting the elements own width ensure its centered above the chromosome
+            return position - (this.clientWidth / 2);
+        })
 
     let markerCoordinatesMap = {};
 
